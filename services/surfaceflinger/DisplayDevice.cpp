17c802ff94770ea052c850468a8e88b6714b4c08
Voyager2k <clego22284@aol.com>
2014-03-03 00:02:34 -0800

    Add legacy framebuffer support This is needed for devices that never got updated past 4.1. Add "BOARD_EGL_NEEDS_FNW := true" to your BoardConfig to use
    Change-Id: Ieac71e2d937c56be4db5be3d649b86ea85f460d7

--- services/surfaceflinger/DisplayDevice.cpp
+++ services/surfaceflinger/DisplayDevice.cpp
@@ -29,6 +29,14 @@
 
 #include <gui/Surface.h>
 
+#ifdef BOARD_EGL_NEEDS_FNW
+#include <ui/FramebufferNativeWindow.h>
+#endif
+
+#include <GLES/gl.h>
+#include <EGL/egl.h>
+#include <EGL/eglext.h>
+
 #include <hardware/gralloc.h>
 
 #include "DisplayHardware/DisplaySurface.h"
@@ -75,7 +83,11 @@ DisplayDevice::DisplayDevice(
       mOrientation()
 {
     mNativeWindow = new Surface(producer, false);
+#ifndef BOARD_EGL_NEEDS_FNW
     ANativeWindow* const window = mNativeWindow.get();
+#else
+    ANativeWindow* const window = new FramebufferNativeWindow();
+#endif
 
     int format;
     window->query(window, NATIVE_WINDOW_FORMAT, &format);
